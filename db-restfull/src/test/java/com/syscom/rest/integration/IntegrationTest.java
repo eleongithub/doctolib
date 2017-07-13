fbbe3d435812909ec7dab2a2690ed8648dba285e
Eric LEGBA <eleongithub@gmail.com>
2017-07-13 19:36:43 +0200

    Ameliorations des test Junit

--- db-restfull/src/test/java/com/syscom/rest/integration/IntegrationTest.java
+++ db-restfull/src/test/java/com/syscom/rest/integration/IntegrationTest.java
@@ -9,6 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
  * Created by ansible on 13/07/17.
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {ConfigIntegration.class})
+@SpringBootTest(classes = {ConfigIntegration.class}) //webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
 public class IntegrationTest {
 }
