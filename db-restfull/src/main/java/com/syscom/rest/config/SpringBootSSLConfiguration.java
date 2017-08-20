a90867ee2d1af4e3c305768893470eccaa4153d4
Eric LEGBA <eleongithub@gmail.com>
2017-08-20 12:58:43 +0200

    Amelioration de code

--- db-restfull/src/main/java/com/syscom/rest/config/SpringBootSSLConfiguration.java
+++ db-restfull/src/main/java/com/syscom/rest/config/SpringBootSSLConfiguration.java
@@ -12,6 +12,10 @@ import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Configuration SSL pour l'application spring boot.
@@ -36,28 +40,49 @@ public class SpringBootSSLConfiguration {
             LOGGER.info("SSL is disabled for Spring boot application");
             return new TomcatEmbeddedServletContainerFactory();
         }
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory(){
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
+//        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory(){
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint securityConstraint = new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
         tomcat.addAdditionalTomcatConnectors(createSslConnector());
         return tomcat;
     }
 
     private Connector createSslConnector() {
-       Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-       Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
-       connector.setScheme("http");
-       connector.setSecure(true);
-       connector.setPort(8443);
-       protocol.setSSLEnabled(true);
-       return connector;
+//       Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//       Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+//       connector.setScheme("https");
+//       connector.setSecure(true);
+//       connector.setPort(8443);
+//       protocol.setSSLEnabled(true);
+//       return connector;
+
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+        try {
+            File keystore = new ClassPathResource("keystore.jks").getFile();
+            File truststore = new ClassPathResource("keystore.jks").getFile();
+            connector.setScheme("https");
+            connector.setSecure(true);
+            connector.setPort(8443);
+            protocol.setSSLEnabled(true);
+            protocol.setKeystoreFile(keystore.getAbsolutePath());
+            protocol.setKeystorePass("password");
+            protocol.setTruststoreFile(truststore.getAbsolutePath());
+            protocol.setTruststorePass("password");
+            protocol.setKeyAlias("keyAlias");
+            return connector;
+        }catch (IOException ex) {
+            throw new IllegalStateException("can't access keystore: [" + "keystore"
+                    + "] or truststore: [" + "keystore" + "]", ex);
+        }
     }
 }
